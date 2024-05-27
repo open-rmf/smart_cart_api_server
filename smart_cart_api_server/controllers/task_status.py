0245bc1a664ce3c7dc48b21866fbd9a502310219
Arjo Chakravarty <arjoc@google.com>
2024-05-27 10:35:30 +0800

    Fix `parse_task_status` and get the locations correctly.
    Signed-off-by: Arjo Chakravarty <arjoc@google.com>

--- smart_cart_api_server/controllers/task_status.py
+++ smart_cart_api_server/controllers/task_status.py
@@ -1,34 +1,50 @@
-from api_server.models.rmf_api.task_state import TaskState
+from api_server.models.rmf_api.task_state import TaskState, Status
 from smart_cart_api_server.models.task_status import TaskStatus, TaskDestination
 import json
 import datetime
 from parse import parse
 
 def parse_task_status(task_state: str) -> TaskStatus:
+    """
+    This function parses a task state coming in from a json string into the robots current location.
+    Note: This function makes some pretty strong assumptions about the task structure. Namely that:
+    - Every "Perform Action" is a pickup/dropoff location
+    - All  pickups/drop offs can be gathered by the GoToPlace command.
+    """
     tasks = json.loads(task_state)
     state = TaskState.model_validate(tasks[0])
 
     locations = []
     loc_idxs = {}
+    next_loc_idx = {}
     for p in sorted(state.phases.keys()):
         ### LOTS OF MAGIC
         x = json.loads(str(state.phases[p].detail)[6:-1])[0]
         if x["category"] == "Perform action":
             res = parse("Performing action wait_until at waypoint [[place:{place}]]", x["detail"])
-            loc_idxs[len(locations)] = res["place"]
+            loc_idxs[p] = len(locations)
             locations.append(TaskDestination(
                 name=res["place"],
                 compartment=None,
                 action="dropoff"
             ))
+        else:
+            next_loc_idx[p] = len(locations)
 
 
     current_location = None
-    if state.status == "underway" and state.active is not None:
+    traveling_to = None
+
+    if state.status == Status.underway and state.active is not None:
         #hack
         st = str(state.active)[5:]
-        if isinstance(state.phases[st].detail, str) and state.phases[st].detail.startswith("Performing action"):
-            current_location =int(st)
+        detail = json.loads(str(state.phases[st].detail)[6:-1])[0]
+        if detail["category"] == "Perform action":
+            current_location = loc_idxs[st]
+        else:
+            next_idx = next_loc_idx[st]
+            if next_idx < len(locations):
+                traveling_to = next_idx
 
     return TaskStatus(
         taskId=state.booking.id,
@@ -39,7 +55,7 @@ def parse_task_status(task_state: str) -> TaskStatus:
         cartId=state.assigned_to.name, # For now only use cart_id
         destinations=locations,
         currentLocationIndex=current_location,
-        #travellingToIndex=,
+        travellingToIndex=traveling_to,
         authorizedDepartures=[],
         unauthorizedDepartures=[],
         status=state.status
--- smart_cart_api_server/test/test_task_status.py
+++ smart_cart_api_server/test/test_task_status.py
@@ -43,3 +43,25 @@ class TestDataSummary(unittest.TestCase):
         self.assertEqual(task_status.cartId, "cssdmir001")
         self.assertEqual(task_status.status, "completed")
         self.assertEqual(task_status.currentLocationIndex, None)
+        self.assertEqual(task_status.travellingToIndex, None)
+
+    def test_parse_underway_last_item_task_status(self):
+        test_string = {'active': 2, 'assigned_to': {'group': 'mir_mc_cssd', 'name': 'cssdmir001'}, 'booking': {'id': 'compose.dispatch-16', 'unix_millis_earliest_start_time': 0}, 'category': 'rmf_milkrun_delivery', 'completed': [1], 'detail': '', 'estimate_millis': 50660, 'original_estimate_millis': 60000, 'pending': [], 'phases': {'1': {'category': 'Sequence', 'detail': '[{"category":"Go to [place:MB_B1_Part_A_Entry]","detail":"Moving the robot from [place:MC_CSSD_Charger_1] to [place:MB_B1_Part_A_Entry]"}]', 'estimate_millis': 4, 'events': {'0': {'deps': [1], 'detail': '', 'id': 0, 'name': 'Sequence', 'status': 'completed'}, '1': {'deps': [2], 'detail': 'Moving the robot from [place:MC_CSSD_Charger_1] to [place:MB_B1_Part_A_Entry]', 'id': 1, 'name': 'Go to [place:MB_B1_Part_A_Entry]', 'status': 'completed'}, '2': {'deps': [], 'detail': '', 'id': 2, 'name': 'Move to [place:MB_B1_Part_A_Entry] < 181.669 -59.4866  -1.5708> through 3 points', 'status': 'completed'}}, 'final_event_id': 0, 'id': 1, 'original_estimate_millis': 53450, 'unix_millis_finish_time': 1949330, 'unix_millis_start_time': 1894190}, '2': {'category': 'Sequence', 'detail': '[{"category":"Perform action","detail":"Performing action wait_until at waypoint [[place:MB_B1_Part_A_Entry]]"}]', 'estimate_millis': 50660, 'events': {'0': {'deps': [1], 'detail': '', 'id': 0, 'name': 'Sequence', 'status': 'underway'}, '1': {'deps': [], 'detail': 'Performing action wait_until at waypoint [[place:MB_B1_Part_A_Entry]]', 'id': 1, 'name': 'Perform action', 'status': 'underway'}}, 'final_event_id': 0, 'id': 2, 'original_estimate_millis': 60000, 'unix_millis_start_time': 1949330}}, 'status': 'underway', 'unix_millis_finish_time': 2009330, 'unix_millis_start_time': 1894210}
+        task_status = parse_task_status(json.dumps([test_string]))
+        self.assertEqual(task_status.authorizedDepartures, [])
+        self.assertEqual(task_status.destinations, [TaskDestination(name='MB_B1_Part_A_Entry', compartment=None, action='dropoff')])
+        self.assertEqual(task_status.cartId, "cssdmir001")
+        self.assertEqual(task_status.status, "underway")
+        self.assertEqual(task_status.currentLocationIndex, 0)
+        self.assertEqual(task_status.travellingToIndex, None)
+
+    def test_parse_underway_travelling_task_status(self):
+        test_string = {'active': 1, 'assigned_to': {'group': 'mir_mc_cssd', 'name': 'cssdmir001'}, 'booking': {'id': 'compose.dispatch-16', 'unix_millis_earliest_start_time': 0}, 'category': 'rmf_milkrun_delivery', 'completed': [], 'detail': '', 'estimate_millis': 109421, 'original_estimate_millis': 60000, 'pending': [2], 'phases': {'1': {'category': 'Sequence', 'detail': '[{"category":"Go to [place:MB_B1_Part_A_Entry]","detail":"Moving the robot from [place:MC_CSSD_Charger_1] to [place:MB_B1_Part_A_Entry]"}]', 'estimate_millis': 49421, 'events': {'0': {'deps': [1], 'detail': '', 'id': 0, 'name': 'Sequence', 'status': 'underway'}, '1': {'deps': [2], 'detail': 'Moving the robot from [place:MC_CSSD_Charger_1] to [place:MB_B1_Part_A_Entry]', 'id': 1, 'name': 'Go to [place:MB_B1_Part_A_Entry]', 'status': 'underway'}, '2': {'deps': [], 'detail': '', 'id': 2, 'name': 'Move to [place:MB_B1_Part_A_Entry] < 181.669 -59.4866  -1.5708> through 3 points', 'status': 'underway'}}, 'final_event_id': 0, 'id': 1, 'original_estimate_millis': 53450, 'unix_millis_start_time': 1894190}, '2': {'category': 'Sequence', 'detail': '[{"category":"Perform action","detail":"Performing action wait_until at waypoint [[place:MB_B1_Part_A_Entry]]"}]', 'estimate_millis': 60000, 'id': 2}}, 'status': 'underway', 'unix_millis_finish_time': 2004631, 'unix_millis_start_time': 1894210}
+        task_status = parse_task_status(json.dumps([test_string]))
+        self.assertEqual(task_status.authorizedDepartures, [])
+        self.assertEqual(task_status.destinations, [TaskDestination(name='MB_B1_Part_A_Entry', compartment=None, action='dropoff')])
+        self.assertEqual(task_status.cartId, "cssdmir001")
+        self.assertEqual(task_status.status, "underway")
+        self.assertEqual(task_status.currentLocationIndex, None)
+        self.assertEqual(task_status.travellingToIndex, 0)
+
