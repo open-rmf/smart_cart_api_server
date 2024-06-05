1c233f338b2043de50162745c6d33c17456971b1
Teo Koon Peng <teokoonpeng@gmail.com>
2024-06-05 15:41:14 +0800

    add params
    Signed-off-by: Teo Koon Peng <teokoonpeng@gmail.com>

--- smart_cart_api_server/__main__.py
+++ smart_cart_api_server/__main__.py
@@ -1,12 +1,35 @@
+import argparse
+
 import uvicorn
+
 from smart_cart_api_server.endpoints.endpoints import app
+import smart_cart_api_server.endpoints.endpoints as ep
 
 
 def main():
+    parser = argparse.ArgumentParser(description="Smart Cart API Server")
+    parser.add_argument(
+        "--host", help="Host to bind to, defaults to 0.0.0.0", default="0.0.0.0"
+    )
+    parser.add_argument(
+        "--port",
+        help="Port to bind to, defaults to 9090",
+        default=9090,
+    )
+    parser.add_argument(
+        "--rmf_api_server_url",
+        help="RMF API Server URL, defaults to http://localhost:8000, may also be set with `API_SERVER_URL` env",
+        default="http://localhost:8000",
+    )
+    args = parser.parse_args()
+
+    if args.rmf_api_server_url:
+        ep.api_server_url = args.rmf_api_server_url
+
     uvicorn.run(
         app,
-        host="0.0.0.0",
-        port=9090,
+        host=args.host,
+        port=args.port,
     )
 
 
--- smart_cart_api_server/endpoints/endpoints.py
+++ smart_cart_api_server/endpoints/endpoints.py
@@ -1,5 +1,8 @@
 from smart_cart_api_server.models.token import Credentials, TokenResponse
-from smart_cart_api_server.models.cart_authorization import AuthorizationRequest, AuthorizationResponse
+from smart_cart_api_server.models.cart_authorization import (
+    AuthorizationRequest,
+    AuthorizationResponse,
+)
 from smart_cart_api_server.models.cart_status import CartStatus
 from smart_cart_api_server.models.destination_complete import DestinationComplete
 from smart_cart_api_server.models.robot_status import RobotStatus
@@ -9,8 +12,12 @@ from smart_cart_api_server.models.tables.CardIdADIDTable import CSVCardIdADIDTab
 
 from smart_cart_api_server.controllers.robot_status import get_robot_status
 from smart_cart_api_server.controllers.task_status import get_task_status
-from smart_cart_api_server.controllers.compartment_authorization import get_compartment_authorization
-from smart_cart_api_server.controllers.destination_complete import notify_rmf_destination_complete
+from smart_cart_api_server.controllers.compartment_authorization import (
+    get_compartment_authorization,
+)
+from smart_cart_api_server.controllers.destination_complete import (
+    notify_rmf_destination_complete,
+)
 
 from smart_cart_api_server.keycloak.parse_config import keycloak_from_json
 
@@ -36,22 +43,34 @@ if "ENABLE_KEYCLOACK" in os.environ:
 # Connect to CardID Table
 card_table = CSVCardIdADIDTable("CardId.csv")
 
+
 # Authentication (placeholder)
 def verify_token(token: str):
     # Your actual token verification logic
     return True
 
+
 @app.post("/compartment_authorization")
-async def request_compartment_authorization(data: AuthorizationRequest, token: Annotated[str, Header()]) -> AuthorizationResponse:
+async def request_compartment_authorization(
+    data: AuthorizationRequest, token: Annotated[str, Header()]
+) -> AuthorizationResponse:
     if not verify_token(token):
         raise HTTPException(status_code=401, detail="Unauthorized")
 
     authorization_response = get_compartment_authorization(
-        card_table, keycloak_connection, data.cartId, data.cardId, api_server=api_server_url)
+        card_table,
+        keycloak_connection,
+        data.cartId,
+        data.cardId,
+        api_server=api_server_url,
+    )
     return authorization_response
 
+
 @app.post("/cart_status_update")
-async def update_cart_status(cart_status: CartStatus, token: Annotated[str, Header()]) -> CartStatus:
+async def update_cart_status(
+    cart_status: CartStatus, token: Annotated[str, Header()]
+) -> CartStatus:
     if not verify_token(token):
         raise HTTPException(status_code=401, detail="Unauthorized")
 
@@ -63,21 +82,28 @@ async def update_cart_status(cart_status: CartStatus, token: Annotated[str, Head
     # Return a success response
     return cart_status  # Echo back the received data
 
+
 @app.post("/destination_complete")
-async def handle_destination_complete(data: DestinationComplete, token: Annotated[str, Header()]):
+async def handle_destination_complete(
+    data: DestinationComplete, token: Annotated[str, Header()]
+):
     if not verify_token(token):
         raise HTTPException(status_code=401, detail="Unauthorized")
     try:
-        await notify_rmf_destination_complete(data.cartId, data.completedDestination, data.success, api_server_url)
+        await notify_rmf_destination_complete(
+            data.cartId, data.completedDestination, data.success, api_server_url
+        )
         return data  # Echo back the data
     except Exception as e:
         # Proper error handling if communication with RMF fails
-        raise HTTPException(status_code=500, detail=f"Error communicating with RMF: {str(e)}")
-
+        raise HTTPException(
+            status_code=500, detail=f"Error communicating with RMF: {str(e)}"
+        )
 
 
 @app.get("/robot_status/{robot_id}")
 async def get_status(robot_id: str, token: Annotated[str, Header()]) -> RobotStatus:
+    print(api_server_url)
     if not verify_token(token):
         raise HTTPException(status_code=401, detail="Unauthorized")
     try:
@@ -85,11 +111,15 @@ async def get_status(robot_id: str, token: Annotated[str, Header()]) -> RobotSta
         return robot_status
     except Exception as e:
         # Handle potential errors when communicating with RMF
-        raise HTTPException(status_code=500, detail=f"Error retrieving robot status from RMF: {str(e)}")
+        raise HTTPException(
+            status_code=500, detail=f"Error retrieving robot status from RMF: {str(e)}"
+        )
 
 
 @app.get("/task_status/{task_id}")
-async def retrieve_task_status(task_id: str, token: Annotated[str, Header()]) -> TaskStatus:
+async def retrieve_task_status(
+    task_id: str, token: Annotated[str, Header()]
+) -> TaskStatus:
     if not verify_token(token):
         raise HTTPException(status_code=401, detail="Unauthorized")
 
@@ -98,16 +128,22 @@ async def retrieve_task_status(task_id: str, token: Annotated[str, Header()]) ->
         return task_status
     except Exception as e:
         # Handle potential errors when communicating with RMF
-        raise HTTPException(status_code=500, detail=f"Error retrieving task status from RMF: {str(e)}")
-
-
+        raise HTTPException(
+            status_code=500, detail=f"Error retrieving task status from RMF: {str(e)}"
+        )
 
 
 @app.exception_handler(HTTPException)
 async def http_exception_handler(request, exc):
-    return JSONResponse({"message": str(exc.detail), "code": exc.status_code}, status_code=exc.status_code)
+    return JSONResponse(
+        {"message": str(exc.detail), "code": exc.status_code},
+        status_code=exc.status_code,
+    )
 
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request, exc):
-    return JSONResponse({"message": str(exc.detail), "code": exc.status_code}, status_code=exc.status_code)
+    return JSONResponse(
+        {"message": str(exc.detail), "code": exc.status_code},
+        status_code=exc.status_code,
+    )
