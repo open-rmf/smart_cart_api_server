9cba63a65f1afcd0efa8546e139eb6fe2465d68b
Arjo Chakravarty <arjoc@google.com>
2024-07-09 11:53:47 +0800

    Fix incorrect task order
    Signed-off-by: Arjo Chakravarty <arjoc@google.com>

--- smart_cart_api_server/controllers/task_status.py
+++ smart_cart_api_server/controllers/task_status.py
@@ -78,8 +78,8 @@ def parse_task_status(task_state: str) -> TaskStatus | None:
             status=state.status.value,
             taskType=task_type
         )
-
-    for p in sorted(state.phases.keys()):
+    print(state.phases.keys())
+    for p in sorted(state.phases.keys(), key= lambda x: int(x)):
         ### LOTS OF MAGIC
         x = json.loads(state.phases[p].detail.__root__)[0]
         if x["category"] == "Perform action":
