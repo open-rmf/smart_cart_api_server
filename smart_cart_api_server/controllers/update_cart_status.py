6a2678a15e5ada3ac5a55f3bc4253fe4b4d14791
Arjo Chakravarty <arjoc@google.com>
2024-07-11 16:41:24 +0800

    Address pr feedback
    Signed-off-by: Arjo Chakravarty <arjoc@google.com>

--- smart_cart_api_server/controllers/update_cart_status.py
+++ smart_cart_api_server/controllers/update_cart_status.py
@@ -1,6 +1,5 @@
 from smart_cart_api_server.models.cart_status import CartStatus
 from urllib.parse import urljoin
-#from api_server.models.alerts import Al
 import aiohttp
 from fastapi import HTTPException
 
@@ -12,7 +11,7 @@ async def update_cart_status(cart_status: CartStatus, api_server: str, headers: 
         data = {
             "id": cart_status.cartId,
             "type": "smart_cart",
-            "data": json.loads(cart_status.json()) #horrible hack cause we are still on pydantic 1.x. In pydantic 2.x we can use dict(rountrip=true)
+            "data": json.loads(cart_status.json()) #horrible hack cause we are still on pydantic 1.x. In pydantic 2.x we can use model_dump(round_trip=True)
         }
         async with session.put(urljoin(api_server, f"/rios"), headers={"Content-Type": "application/json"}, json=data) as response:
             if response.status != 200 and response.status != 201:
