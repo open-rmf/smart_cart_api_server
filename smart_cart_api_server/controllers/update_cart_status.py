4ba4b44ce54886e2755015a1d0288c2881079c92
Arjo Chakravarty <arjoc@google.com>
2024-07-11 15:36:49 +0800

    Implements smartcart updates via RIO
    As said abaove.
    
    Signed-off-by: Arjo Chakravarty <arjoc@google.com>

---
+++ smart_cart_api_server/controllers/update_cart_status.py
@@ -0,0 +1,24 @@
+from smart_cart_api_server.models.cart_status import CartStatus
+from urllib.parse import urljoin
+#from api_server.models.alerts import Al
+import aiohttp
+from fastapi import HTTPException
+
+import json
+
+async def update_cart_status(cart_status: CartStatus, api_server: str, headers: dict[str, str]) -> CartStatus:
+    print(cart_status.json())
+    async with aiohttp.ClientSession(headers=headers) as session:
+        data = {
+            "id": cart_status.cartId,
+            "type": "smart_cart",
+            "data": json.loads(cart_status.json()) #horrible hack cause we are still on pydantic 1.x. In pydantic 2.x we can use dict(rountrip=true)
+        }
+        async with session.put(urljoin(api_server, f"/rios"), headers={"Content-Type": "application/json"}, json=data) as response:
+            if response.status != 200 and response.status != 201:
+                print(response.status)
+                raise HTTPException(
+                    status_code=500, detail=f"got error from remote server: {await response.text()}"
+                )
+            print(await response.text())
+            return cart_status
